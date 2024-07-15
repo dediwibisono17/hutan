049bc97fa9b15296e58ba3323460b42b4de70a00
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-07-15 08:47:29 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+$(document).ready(function () {
+
+    new WOW().init();
+})
+
 $('[data-toggle="tooltip"]').tooltip()
 
 /*Highcharts.chart('container', {
@@ -209,4 +214,36 @@ $('.slider-peta').slick({
     speed: 500,
     cssEase: 'cubic-bezier(0.600, -0.280, 0.735, 0.045)',
     // cssEase: 'linear'
+});
+
+
+
+const pauseVar = "fa-pause-circle";
+const playVar = "fa-play-circle";
+
+const btns = document.querySelectorAll(".all");
+
+function onChange(event) {
+    const buttonElement = event.currentTarget;
+
+    const isPlayButton = buttonElement.classList.contains(playVar);
+
+    if (isPlayButton) {
+        buttonElement.classList.remove(playVar);
+        buttonElement.classList.add(pauseVar);
+        $("#play-button").removeClass("animate__infinite");
+        $("#myAudio").get(0).play();
+    } else {
+        buttonElement.classList.remove(pauseVar);
+        buttonElement.classList.add(playVar);
+        $("#play-button").addClass("animate__infinite");
+        $("#myAudio").get(0).pause();
+    }
+
+    // You can also use .toggle function on classList as mentioned by the person in other answer
+}
+
+// query selector all returns a list of nodes, therefore we need to iterate over it and attach an event listener to each button seperatly
+btns.forEach((btn) => {
+    btn.addEventListener("click", onChange);
 });
