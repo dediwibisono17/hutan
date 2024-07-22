8224caba95147440a65ba6fecef6c437af60a0b5
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-07-22 14:33:07 +0700

    gtg

--- js/data-highcharts.js
+++ js/data-highcharts.js
@@ -4,65 +4,40 @@ Highcharts.setOptions({
     }
 })
 
+// Data retrieved from: https://ferjedatabanken.no/statistikk
 Highcharts.chart('container', {
     chart: {
         type: 'bar'
     },
     title: {
-        text: 'Historic World Population by Region',
-        align: 'left'
+        text: 'Tumpang Tindih Wilayah Adat dengan Izin Usaha'
     },
     subtitle: {
-        text: 'Source: <a ' +
-            'href="https://en.wikipedia.org/wiki/List_of_continents_and_continental_subregions_by_population"' +
-            'target="_blank">Wikipedia.org</a>',
-        align: 'left'
+        text: '<small>Source: AMAN</small>',
+        align: 'center'
     },
     xAxis: {
         categories: ['Sumatera', 'Kalimantan', 'Jawa-Bali-Nusa', 'Sulawesi', 'Maluku', 'Papua'],
-        title: {
-            text: null
-        },
-        gridLineWidth: 1,
-        lineWidth: 0
     },
     yAxis: {
         min: 0,
         title: {
-            text: '(dalam Hektare)',
-            align: 'bottom'
-        },
-        labels: {
-            overflow: 'justify'
-        },
-        gridLineWidth: 0
+            text: ''
+        }
+    },
+    legend: {
+        reversed: true
     },
     tooltip: {
         valueSuffix: ' hektare'
     },
     plotOptions: {
-        bar: {
-            borderRadius: '50%',
+        series: {
+            stacking: 'normal',
             dataLabels: {
                 enabled: true
-            },
-            groupPadding: 0.1
+            }
         }
-    },
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'top',
-        x: -40,
-        y: 80,
-        floating: true,
-        borderWidth: 1,
-        backgroundColor:
-            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
-        shadow: true
-    },
-    credits: {
-        enabled: false
     },
     series: [{
         name: 'HPH',
