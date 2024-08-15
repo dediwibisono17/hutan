4d98ee81937b52b9ae32ba7478ba17ce023f3785
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-08-15 10:29:14 +0700

    iupDTE

--- js/highcharts-aman.js
+++ js/highcharts-aman.js
@@ -1,68 +1,111 @@
 Highcharts.chart('container-aman', {
     chart: {
         type: 'pie',
-        backgroundColor: "transparent",
-    },
-    title: {
-        text: 'Hak Pengusahaan Hutan',
-        style: {
-            // color: "#fafafa",
-            font: 'bold 20px "Rubik", sans-serif',
+        custom: {},
+        backgroundColor: 'rgba(255,255,255,0)',
+        borderRadius: 16,
+        events: {
+            render() {
+                const chart = this,
+                    series = chart.series[0];
+                let customLabel = chart.options.chart.custom.label;
 
+                points = chart.series[0].points,
+                    len = points.length,
+                    total = 0,
+                    i = 0;
+
+                for (; i < len; i++) {
+                    total += points[i].y;
+                }
+                if (!customLabel) {
+                    customLabel = chart.options.chart.custom.label =
+                        chart.renderer.label(
+                            'Total<br/>' +
+                            '<strong>' + total.toLocaleString('id') + '</strong> ha'
+                        )
+                            .css({
+                                // color: '#fff',
+                                font: 'normal 2rem "Rubik", sans-serif',
+                                textAnchor: 'middle'
+                            })
+                            .add();
+                }
+
+                const x = series.center[0] + chart.plotLeft,
+                    y = series.center[1] + chart.plotTop -
+                        (customLabel.attr('height') / 2);
+
+                customLabel.attr({
+                    x,
+                    y
+                });
+                // Set font size based on chart diameter
+                customLabel.css({
+                    fontSize: `${series.center[2] / 12}px`
+                });
+            }
         }
     },
+    title: {
+        text: 'Hak Pengusahaan Hutan'
+    },
+    subtitle: {
+        text: null
+    },
     tooltip: {
-        valueSuffix: ' hektare',
+        pointFormat: '{series.name}: <b>{point.y}</b> ha',
         style: {
             //fontWeight: 'bold',
             fontFamily: 'Rubik',
             fontSize: '1rem'
         }
     },
-    subtitle: {
-        text: null
+    legend: {
+        enabled: false
     },
     credits: {
         enabled: false //buat highcharts com
     },
     plotOptions: {
-        series: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: [{
-                enabled: true,
-                distance: 20,
+        pie: {
+            borderColor: 'transparent',
+            dataLabels: {
                 style: {
                     // color: "white",//color nama
-                    borderColor: 'transparent',
-                    font: 'normal 1.3rem "Rubik", sans-serif',
+                    // borderColor: '#fafafa',
+                    font: 'normal 1rem "Rubik", sans-serif',
                     fontWeight: 'normal',
                     textOutline: false
                     // lineHeight: '2rem'
                 },
+            }
+        },
+        series: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            borderRadius: 0,
+            dataLabels: [{
+                enabled: true,
+                distance: 20,
+                format: '{point.name}'
             }, {
                 enabled: true,
-                distance: -40,
-                // format: '{point.percentage:.1f}%',
+                distance: -10,
                 format: '{point.y:,.0f}',
                 style: {
-                    fontSize: '1.2em',
-                    textOutline: 'none',
-                    opacity: 0.7,
-                    fontFamily: 'Rubik'
-                },
-                filter: {
-                    operator: '>',
-                    property: 'percentage',
-                    value: 10
+                    fontSize: '0.9em',
+                    // color: "#fafafa"
                 }
-            }]
+            }],
+            showInLegend: false
         }
     },
     series: [
         {
             name: 'Luas',
             colorByPoint: true,
+            innerSize: '65%',
             // data: [182156, 2445928, 0, 56767, 18594, 1551025],
             data: [
                 {
@@ -94,4 +137,425 @@ Highcharts.chart('container-aman', {
             ]
         }
     ]
+});
+
+Highcharts.chart('container-aman-hti', {
+    chart: {
+        type: 'pie',
+        custom: {},
+        backgroundColor: 'rgba(255,255,255,0)',
+        borderRadius: 16,
+        events: {
+            render() {
+                const chart = this,
+                    series = chart.series[0];
+                let customLabel = chart.options.chart.custom.label;
+
+                points = chart.series[0].points,
+                    len = points.length,
+                    total = 0,
+                    i = 0;
+
+                for (; i < len; i++) {
+                    total += points[i].y;
+                }
+                if (!customLabel) {
+                    customLabel = chart.options.chart.custom.label =
+                        chart.renderer.label(
+                            'Total<br/>' +
+                            '<strong>' + total.toLocaleString('id') + '</strong> ha'
+                        )
+                            .css({
+                                // color: '#fff',
+                                font: 'normal 2rem "Rubik", sans-serif',
+                                textAnchor: 'middle'
+                            })
+                            .add();
+                }
+
+                const x = series.center[0] + chart.plotLeft,
+                    y = series.center[1] + chart.plotTop -
+                        (customLabel.attr('height') / 2);
+
+                customLabel.attr({
+                    x,
+                    y
+                });
+                // Set font size based on chart diameter
+                customLabel.css({
+                    fontSize: `${series.center[2] / 12}px`
+                });
+            }
+        }
+    },
+    title: {
+        text: 'Hutan Tanaman Industri'
+    },
+    subtitle: {
+        text: null
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.y}</b> ha',
+        style: {
+            //fontWeight: 'bold',
+            fontFamily: 'Rubik',
+            fontSize: '1rem'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    credits: {
+        enabled: false //buat highcharts com
+    },
+    plotOptions: {
+        pie: {
+            borderColor: 'transparent',
+            dataLabels: {
+                style: {
+                    // color: "white",//color nama
+                    // borderColor: '#fafafa',
+                    font: 'normal 1rem "Rubik", sans-serif',
+                    fontWeight: 'normal',
+                    textOutline: false
+                    // lineHeight: '2rem'
+                },
+            }
+        },
+        series: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            borderRadius: 0,
+            dataLabels: [{
+                enabled: true,
+                distance: 20,
+                format: '{point.name}'
+            }, {
+                enabled: true,
+                distance: -10,
+                format: '{point.y:,.0f}',
+                style: {
+                    fontSize: '0.9em',
+                    // color: "#fafafa"
+                }
+            }],
+            showInLegend: false
+        }
+    },
+    series: [
+        {
+            name: 'Luas',
+            colorByPoint: true,
+            innerSize: '65%',
+            // data: [399810, 668008, 1293, 16356, 0, 494157],
+            data: [
+                {
+                    name: 'Sumatera',
+                    y: 399810
+                },
+                {
+                    name: 'Kalimantan',
+                    // sliced: true,
+                    // selected: true,
+                    y: 668008
+                },
+                {
+                    name: 'Jawa Bali Nusa',
+                    y: 1293
+                },
+                {
+                    name: 'Sulawesi',
+                    y: 0
+                },
+                {
+                    name: 'Maluku',
+                    y: 494157
+                },
+                {
+                    name: 'Papua',
+                    y: 1551025
+                }
+            ]
+        }
+    ]
+});
+
+
+Highcharts.chart('container-aman-pertambangan', {
+    chart: {
+        type: 'pie',
+        custom: {},
+        backgroundColor: 'rgba(255,255,255,0)',
+        borderRadius: 16,
+        events: {
+            render() {
+                const chart = this,
+                    series = chart.series[0];
+                let customLabel = chart.options.chart.custom.label;
+
+                points = chart.series[0].points,
+                    len = points.length,
+                    total = 0,
+                    i = 0;
+
+                for (; i < len; i++) {
+                    total += points[i].y;
+                }
+                if (!customLabel) {
+                    customLabel = chart.options.chart.custom.label =
+                        chart.renderer.label(
+                            'Total<br/>' +
+                            '<strong>' + total.toLocaleString('id') + '</strong> ha'
+                        )
+                            .css({
+                                // color: '#fff',
+                                font: 'normal 2rem "Rubik", sans-serif',
+                                textAnchor: 'middle'
+                            })
+                            .add();
+                }
+
+                const x = series.center[0] + chart.plotLeft,
+                    y = series.center[1] + chart.plotTop -
+                        (customLabel.attr('height') / 2);
+
+                customLabel.attr({
+                    x,
+                    y
+                });
+                // Set font size based on chart diameter
+                customLabel.css({
+                    fontSize: `${series.center[2] / 12}px`
+                });
+            }
+        }
+    },
+    title: {
+        text: 'Pertambangan'
+    },
+    subtitle: {
+        text: null
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.y}</b> ha',
+        style: {
+            //fontWeight: 'bold',
+            fontFamily: 'Rubik',
+            fontSize: '1rem'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    credits: {
+        enabled: false //buat highcharts com
+    },
+    plotOptions: {
+        pie: {
+            borderColor: 'transparent',
+            dataLabels: {
+                style: {
+                    // color: "white",//color nama
+                    // borderColor: '#fafafa',
+                    font: 'normal 1rem "Rubik", sans-serif',
+                    fontWeight: 'normal',
+                    textOutline: false
+                    // lineHeight: '2rem'
+                },
+            }
+        },
+        series: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            borderRadius: 0,
+            dataLabels: [{
+                enabled: true,
+                distance: 20,
+                format: '{point.name}'
+            }, {
+                enabled: true,
+                distance: -10,
+                format: '{point.y:,.0f}',
+                style: {
+                    fontSize: '0.9em',
+                    // color: "#fafafa"
+                }
+            }],
+            showInLegend: false
+        }
+    },
+    series: [{
+        name: 'Luas',
+        colorByPoint: true,
+        innerSize: '65%',
+        // data: [88746, 551379, 29536, 111156, 39049, 29038],
+        data: [
+            {
+                name: 'Sumatera',
+                y: 88746
+            },
+            {
+                name: 'Kalimantan',
+                // sliced: true,
+                // selected: true,
+                y: 551379
+            },
+            {
+                name: 'Jawa Bali Nusa',
+                y: 29536
+            },
+            {
+                name: 'Sulawesi',
+                y: 111156
+            },
+            {
+                name: 'Maluku',
+                y: 39049
+            },
+            {
+                name: 'Papua',
+                y: 29038
+            }
+        ]
+    }]
+});
+
+
+Highcharts.chart('container-aman-perkebunan', {
+    chart: {
+        type: 'pie',
+        custom: {},
+        backgroundColor: 'rgba(255,255,255,0)',
+        borderRadius: 16,
+        events: {
+            render() {
+                const chart = this,
+                    series = chart.series[0];
+                let customLabel = chart.options.chart.custom.label;
+
+                points = chart.series[0].points,
+                    len = points.length,
+                    total = 0,
+                    i = 0;
+
+                for (; i < len; i++) {
+                    total += points[i].y;
+                }
+                if (!customLabel) {
+                    customLabel = chart.options.chart.custom.label =
+                        chart.renderer.label(
+                            'Total<br/>' +
+                            '<strong>' + total.toLocaleString('id') + '</strong> ha'
+                        )
+                            .css({
+                                // color: '#fff',
+                                font: 'normal 2rem "Rubik", sans-serif',
+                                textAnchor: 'middle'
+                            })
+                            .add();
+                }
+
+                const x = series.center[0] + chart.plotLeft,
+                    y = series.center[1] + chart.plotTop -
+                        (customLabel.attr('height') / 2);
+
+                customLabel.attr({
+                    x,
+                    y
+                });
+                // Set font size based on chart diameter
+                customLabel.css({
+                    fontSize: `${series.center[2] / 12}px`
+                });
+            }
+        }
+    },
+    title: {
+        text: 'Perkebunan'
+    },
+    subtitle: {
+        text: null
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.y}</b> ha',
+        style: {
+            //fontWeight: 'bold',
+            fontFamily: 'Rubik',
+            fontSize: '1rem'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    credits: {
+        enabled: false //buat highcharts com
+    },
+    plotOptions: {
+        pie: {
+            borderColor: 'transparent',
+            dataLabels: {
+                style: {
+                    // color: "white",//color nama
+                    // borderColor: '#fafafa',
+                    font: 'normal 1rem "Rubik", sans-serif',
+                    fontWeight: 'normal',
+                    textOutline: false
+                    // lineHeight: '2rem'
+                },
+            }
+        },
+        series: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            borderRadius: 0,
+            dataLabels: [{
+                enabled: true,
+                distance: 20,
+                format: '{point.name}'
+            }, {
+                enabled: true,
+                distance: -10,
+                format: '{point.y:,.0f}',
+                style: {
+                    fontSize: '0.9em',
+                    // color: "#fafafa"
+                }
+            }],
+            showInLegend: false
+        }
+    },
+    series: [{
+        name: 'Luas',
+        colorByPoint: true,
+        innerSize: '65%',
+        //  data: [242990, 974936, 0, 10811, 0, 246478],
+        data: [
+            {
+                name: 'Sumatera',
+                y: 242990
+            },
+            {
+                name: 'Kalimantan',
+                // sliced: true,
+                // selected: true,
+                y: 974936
+            },
+            {
+                name: 'Jawa Bali Nusa',
+                y: 0
+            },
+            {
+                name: 'Sulawesi',
+                y: 10811
+            },
+            {
+                name: 'Maluku',
+                y: 0
+            },
+            {
+                name: 'Papua',
+                y: 246478
+            }
+        ]
+    }]
 });
