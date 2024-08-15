47ef0176f2b1d7e5ff08ff0d1ae5de7dab7d74c7
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-08-15 15:16:31 +0700

    kkk

--- js/highcharts-aman.js
+++ js/highcharts-aman.js
@@ -1,3 +1,7 @@
+
+Highcharts.setOptions({
+    colors: ['#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4']
+});
 Highcharts.chart('container-aman', {
     chart: {
         type: 'pie',
