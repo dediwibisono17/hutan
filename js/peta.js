bd9973cf94741305b39643a2954464d736169df3
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-08-20 05:29:18 +0700

    disable scroll

--- js/peta.js
+++ js/peta.js
@@ -1,4 +1,5 @@
 const map = L.map('map').setView([-1.2910469065280155, 112.12226540832819], 4.5);
+map.scrollWheelZoom.disable();
 
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
