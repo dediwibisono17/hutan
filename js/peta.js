a7f7b5ebb0560e617fd3e9d0343cf1bea80eedab
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-08-27 15:38:16 +0700

    re

--- js/peta.js
+++ js/peta.js
@@ -11,6 +11,8 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+$('.leaflet-control-attribution').hide()
+
 // const baseballIcon = L.icon({
 // 	iconUrl: 'img/baseball-marker.png',
 // 	iconSize: [32, 37],
